a5c0b98a692842a504dee47d2c8184435c53a8fc
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2025-04-16 21:43:52 +0530

    Function

---
+++ Function_Method/Func1.java
@@ -0,0 +1,67 @@
+// import java.util.*;
+// public class Func1 {
+ 
+//   public static int sumFunc(int a, int b){
+//     int sum = a + b;
+//     return sum;
+    
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter a");
+//     int a = sc.nextInt();
+//     System.out.println("Enter b");
+//     int b = sc.nextInt();
+    
+//     int res = sumFunc(a, b);
+//     System.out.println("sum is "+ res);
+//   }
+// }
+                                                          // *********  Multiply Function  *************//
+
+// import java.util.*;
+// public class Func1{
+//   public static int multiplyFunc(int a , int b){
+//     int mul = a*b;
+//     return mul;
+//   }
+
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner (System.in);
+//     System.out.println("Enter a");
+//     int a = sc.nextInt();
+//     System.out.println("Enter b");
+//     int b = sc.nextInt();
+
+//     int multiply = multiplyFunc(a, b);
+//     System.out.println("Multiply is " + multiply);
+//     sc.close();
+//   }
+// }
+
+//                                                              *************   Factotial   ***********
+
+// import java.util.*;
+
+// public class Func1{
+//   public static int Fact(int num){
+//     if(num <0){
+//      System.out.println("invalid Number");
+//     }
+//     int Factorial = 1;
+//     for(int i=num ; i>=1; i--){
+//       Factorial = Factorial*i;
+//     }
+//     System.out.println("Factorial is " + Factorial);
+//     return 0;
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter the number :");
+//     int num = sc.nextInt();
+
+//     Fact(num);
+    
+//     sc.close();
+//   }
+// }
